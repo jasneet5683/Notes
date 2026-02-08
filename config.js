5a48d48c720c4a6972be50555b089f26a4958be3
jasneet5683 <jasneet5683@users.noreply.github.com>
2026-02-08 18:45:15 +0000

    deploy: a2f02a35985c412cdf678b7da741d988fff16e73

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Railway deployment URL
-const API_BASE_URL = 'https://web-production-b8ca4.up.railway.app/api';
+const API_BASE_URL = 'https://notes-production-7134.up.railway.app/api';
 
 export { API_BASE_URL };
