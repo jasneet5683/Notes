6fa45cfa411fe9a3935e73aa46e71ad6a36f596a
jasneet5683 <jasneet.happy@gmail.com>
2026-02-19 18:05:31 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -165,7 +165,7 @@ document.getElementById('taskForm').addEventListener('submit', async (e) => {
         status: document.getElementById('status').value,
         priority: String(document.getElementById('priority').value || "1"),
         notify_email: document.getElementById('notifyEmail').value.trim() || null,
-        predecessor: document.getElementById('successor').value 
+        predecessor: document.getElementById('predecessor').value 
     };
     
     try {
